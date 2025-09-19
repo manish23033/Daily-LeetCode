98b9b0787db6b46bbbd851bac176bbfa076fbc9e
Manish kumar agrahari <107737259+manish23033@users.noreply.github.com>
2025-09-19 22:33:53 +0530

    Time: 38 ms (44.06%), Space: 45 MB (65.12%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.java
@@ -0,0 +1,27 @@
+class Solution {
+    public static int[] twoSum(int[] nums, int target) {
+        int n = nums.length; 
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) { 
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j}; 
+                }
+            }
+        }
+        return new int[]{}; 
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{3, 2, 4};
+        int target = 6;
+
+        int[] result = twoSum(nums, target);
+
+        if (result.length == 2) {
+            System.out.println("Indices: " + result[0] + ", " + result[1]);
+            System.out.println("Numbers: " + nums[result[0]] + ", " + nums[result[1]]);
+        } else {
+            System.out.println("No pair found.");
+        }
+    }
+}
