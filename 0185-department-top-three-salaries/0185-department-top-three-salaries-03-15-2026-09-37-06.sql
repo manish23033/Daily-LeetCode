18e6d8b050cff4c41307785fb29e95ae271fdf9e
MANISH KUMAR AGRAHARI <mka9146866@gmail.com>
2026-03-15 09:37:07 +0530

    Time: 894 ms (67.47%), Space: 0B (100%) - LeetHub

---
+++ 0185-department-top-three-salaries/0185-department-top-three-salaries-03-15-2026-09-37-06.sql
@@ -0,0 +1,11 @@
+select department , employee , salary 
+from 
+(
+select d.name as department , e.name as employee, e.salary as salary ,
+dense_rank() over (partition by d.name order by  e.salary desc ) as ranks 
+
+ from employee e 
+ left join department d 
+ on e.departmentID =d.id 
+)  as temp 
+where ranks <=3
