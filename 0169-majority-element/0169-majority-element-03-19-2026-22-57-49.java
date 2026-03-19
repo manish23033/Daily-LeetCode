15bf19e428320e9875e29155c1d3c653dff690ec
MANISH KUMAR AGRAHARI <mka9146866@gmail.com>
2026-03-19 22:57:47 +0530

    Time: 2 ms (70.69%), Space: 55.8 MB (44.6%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element-03-19-2026-22-57-49.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int majorityElement(int[] num) {
+       int fre = 0 ;
+       int n =num.length;
+       int ans = 0 ;
+       for(int i = 0 ; i<n ; i++ ){
+        if(fre==0 ){
+            ans =num[i];
+        }
+        if(ans==num[i]){
+            fre++;
+        }
+        else{
+            fre--;
+        }
+       }
+       int count=0 ;
+       for(int val :num){
+        if(val==ans){
+            count++ ;
+        }
+       }
+       if(count >n/2){
+        return ans ;
+       }
+       else{
+        return -1 ;
+       }
+    }
+}
