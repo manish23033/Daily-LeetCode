465c0ec821dd8cc0d74726e856aadc36124874c7
MANISH KUMAR AGRAHARI <mka9146866@gmail.com>
2026-03-22 22:39:59 +0530

    Time: 3 ms (19.01%), Space: 42.9 MB (53.09%) - LeetHub

---
+++ 0060-permutation-sequence/0060-permutation-sequence-03-22-2026-22-39-59.java
@@ -0,0 +1,30 @@
+class Solution {
+    public String getPermutation(int n, int k) {
+        List<Integer> arr = new ArrayList<>();
+        
+            int fact = 1; 
+            for(int i = 1 ; i< n ; i++){
+                fact =fact*i ;
+                arr.add(i);
+            }
+
+            arr.add(n);
+            k =k-1 ;
+            String ans ="";
+
+            while(true){
+                ans =ans+arr.get(k/fact);
+
+                arr.remove(k/fact);
+
+                if( arr.size() ==0){
+                    break ;
+                }
+                k =k % fact ;
+                fact = fact / arr.size();
+
+            }
+            return ans ;
+
+            }
+}
